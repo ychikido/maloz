98c159ae30349a08a414cecadbdf65e056490e1d
JoKeRCoMeDiaN <52279858+JoKeRCoMeDiaN@users.noreply.github.com>
2020-03-04 14:39:13 +0100

    Update inloggen.php

--- inloggen.php
+++ inloggen.php
@@ -1,47 +1,8 @@
 
-<html>
-<head>
-<title>log in</title>
-<link rel="stylesheet" href="inc/style.css" type="text/css">
-<script language="">
-<!--
-function cursor(){document.login.name.focus();}
-// -->
-</script>
-</head>
-
-<body bgcolor="#FFFFFF" text="#000000" onLoad=cursor()>
-<img src="inc/title.gif" width="308" height="82">
-<blockquote>
-  <p><b>please login</b></p>
-<table border="0" cellspacing="0" cellpadding="2">
-  <form action="login.php" method="post" name=login>
-    <tr>
-      <td>Username</td>
-      <td>
-        <input type="Text" name="name" size="15">
-      </td>
-    </tr>
-    <tr>
-      <td height="6">Password</td>
-      <td height="6">
-        <input type="password" name="password" size="15">
-      </td>
-    </tr>
-    <tr>
-      <td colspan="2">
-        <input type="Submit" name="submit" value="Enter">
-      </td>
-    </tr>
-  </form>
-</table>
-</blockquote>
-</body>
-</html>
 <?php
 include("inc/config.php");
 $connection = mysql_connect($hostname, $user, $pass) or die ("Unable to connect!");
-$query = "SELECT * FROM clients WHERE name = '$name' AND password = PASSWORD('$password')";
+$query = "SELECT * FROM account WHERE name = '$name' AND password = PASSWORD('$password')";
 $result = mysql_db_query($database, $query, $connection);
 if (mysql_num_rows($result) == 1)
     {
@@ -70,7 +31,7 @@ else
     mysql_free_result ($result);    
     mysql_close($connection);
 
-    header("Location: index.htm");
+    header("Location: index.php");
     exit;
     }
 ?>
